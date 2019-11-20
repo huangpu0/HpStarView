ac1a30b13a38c6e0edd89cd1bb4d1b7bc8e653ff
huangpu0 <huangpu0205@163.com>
2019-11-20 11:00:30 +0800

    Initial Commit

---
+++ HpStarViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HpStarViewDemo
+//
+//  Created by 朴子hp on 2019/11/20.
+//  Copyright © 2019 朴子hp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HpStarViewDemoTests/HpStarViewDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  HpStarViewDemoTests.swift
+//  HpStarViewDemoTests
+//
+//  Created by 朴子hp on 2019/11/20.
+//  Copyright © 2019 朴子hp. All rights reserved.
+//
+
+import XCTest
+@testable import HpStarViewDemo
+
+class HpStarViewDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
